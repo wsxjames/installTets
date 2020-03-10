a0fa35568fd0f626cf60ca9d422cb6d8dc3b42d0
wsxjames <wsxjames@163.com>
2020-03-10 08:29:42 -0400

    Initial Commit

---
+++ installTets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  installTets
+//
+//  Created by 吴世欣 on 3/10/20.
+//  Copyright © 2020 James Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
